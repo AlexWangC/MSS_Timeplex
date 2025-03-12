b548005e8c77dff11c4cd5fa095060190276b7ba
botguest <119339547+botguest@users.noreply.github.com>
2025-03-12 16:10:27 -0400

    attempting at resolving compile error

--- Assets/Scripts/Grid/EditorSnapToGridButton.cs
+++ Assets/Scripts/Grid/EditorSnapToGridButton.cs
@@ -1,48 +1,49 @@
+
 using NaughtyAttributes;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
+// #if UNITY_EDITOR
+// using UnityEditor;
+// #endif
 using UnityEngine;
 
-    [InitializeOnLoad]
+    // [InitializeOnLoad]
     public class EditorSnapToGridButton: MonoBehaviour{
 
     private void Awake()
     {
-        SnapAllToGrid();
+        // SnapAllToGrid();
     }
 
-#if UNITY_EDITOR
-
-    static EditorSnapToGridButton()
-    {
-        SceneView.duringSceneGui += OnSceneGUI;
-    }
-
-    private static void OnSceneGUI(SceneView sceneView)
-    {
-        Handles.BeginGUI();
-        GUILayout.BeginArea(new Rect(10, 10, 200, 50)); // Position in Scene View
-
-        if (GUILayout.Button("Snap All to Grid", GUILayout.Height(30)))
-        {
-            SnapAllToGrid();
-        }
-
-        GUILayout.EndArea();
-        Handles.EndGUI();
-    }
-
-    public static void SnapAllToGrid()
-    {
-        scrGridMakerTilted[] panels = Object.FindObjectsByType<scrGridMakerTilted>(FindObjectsSortMode.None);
-        foreach (scrGridMakerTilted panel in panels)
-        {
-            panel.SnapToGrid();
-        }
-
-        //Debug.Log("All objects snapped to grid!");
-    }
-#endif
+// #if UNITY_EDITOR
+//
+//     static EditorSnapToGridButton()
+//     {
+//         SceneView.duringSceneGui += OnSceneGUI;
+//     }
+//
+//     private static void OnSceneGUI(SceneView sceneView)
+//     {
+//         Handles.BeginGUI();
+//         GUILayout.BeginArea(new Rect(10, 10, 200, 50)); // Position in Scene View
+//
+//         if (GUILayout.Button("Snap All to Grid", GUILayout.Height(30)))
+//         {
+//             SnapAllToGrid();
+//         }
+//
+//         GUILayout.EndArea();
+//         Handles.EndGUI();
+//     }
+//
+//     public static void SnapAllToGrid()
+//     {
+//         scrGridMakerTilted[] panels = Object.FindObjectsByType<scrGridMakerTilted>(FindObjectsSortMode.None);
+//         foreach (scrGridMakerTilted panel in panels)
+//         {
+//             panel.SnapToGrid();
+//         }
+//
+//         //Debug.Log("All objects snapped to grid!");
+//     }
+// #endif
 }
 
