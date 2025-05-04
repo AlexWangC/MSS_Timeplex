2adbbb6a8fc900bab6de372922b48ce5a0611b2e
Shengyuezzz <130474950+Shengyuezzz@users.noreply.github.com>
2025-05-04 00:54:40 -0400

    Adding Animation

--- Assets/Scripts/Player/scrPlayerFlip.cs
+++ Assets/Scripts/Player/scrPlayerFlip.cs
@@ -5,43 +5,53 @@ using UnityEngine;
 
 public class scrPlayerFlip : MonoBehaviour
 {
-    public Sprite spriteUp;
-    public Sprite spriteDown;
-    public Sprite spriteLeft;
-    public Sprite spriteRight;
-    
+    private Animator animator;
+    private string currentState;
+
     // Legacy Code.
     //private bool facing_left = true;
     //private float original_x;
 
-    private void Start()
+    private void Awake()
     {
-        if (spriteUp == null || spriteDown == null || spriteLeft == null || spriteRight == null)
+        animator = GetComponent<Animator>();
+        /*if (spriteUp == null || spriteDown == null || spriteLeft == null || spriteRight == null)
         {
             throw new NullReferenceException("bruh scrPlayerFlip needs all 4 sprites assigned to work. You lazy ass.");
-        }
+        }*/
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            GetComponent<SpriteRenderer>().sprite = spriteLeft;
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            GetComponent<SpriteRenderer>().sprite =　spriteRight;
-        }
-
         if (Input.GetKeyDown(KeyCode.W))
         {
-            GetComponent<SpriteRenderer>().sprite = spriteUp;
+            ChangeState("Child_Up");
         }
-
-        if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.S))
         {
-            GetComponent<SpriteRenderer>().sprite = spriteDown;
+            ChangeState("Child_Down");
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            ChangeState("Child_Left");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            ChangeState("Child_Right");
+        }
+    }
+    private void ChangeState(string newState)
+    {
+        if (currentState == newState) return; 
+
+        if (animator.HasState(0, Animator.StringToHash(newState)))
+        {
+            animator.Play(newState);
+            currentState = newState;
+        }
+        else
+        {
+            Debug.LogWarning($"Animator state '{newState}' not found on {gameObject.name}");
         }
     }
 }
